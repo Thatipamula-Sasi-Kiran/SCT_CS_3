b1122714750f2249d7cd3cd9fec38a2bcf2265af
Thatipamula Sasi Kiran <sasitsk0804@gmail.com>
2026-06-27 18:49:04 +0530

    Enhance password validation in checkPassword function with detailed feedback

--- Program.cpp
+++ Program.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 
 void checkPassword(std::string pass){
     int pScore = 0;
@@ -15,6 +16,51 @@ void checkPassword(std::string pass){
         return ;
     }
 
+    for (int i = 0; i < pass.length(); i++) {
+        //char c = pass[i];
+
+        if (std::isupper(pass[i])){
+            hasUpper = true;
+        }
+        else if (std::islower(pass[i])){
+            hasLower = true;
+        }
+        else if (std::isdigit(pass[i])){
+            hasDigit = true;
+        }
+        else if (std::ispunct(pass[i])){
+            hasSpecial = true;
+        }
+    }
+
+    if (hasUpper == true){ 
+        pScore++;
+    }
+    else {
+        std::cout << "Try to enter an uppercase letter, you fucking idiot!!!\n"; return ;
+    }
+
+    if (hasLower == true){
+         pScore++;
+    }
+    else {
+        std::cout << "Try to enter a lowercase letter, you fucking idiot!!!\n"; return ;
+    }
+    
+    if (hasDigit == true) {
+        pScore++;
+    }
+    else {
+        std::cout << "Try to enter a digit, you fucking idiot!!!\n"; return ;
+    }
+
+    if (hasSpecial == true) {
+        pScore++;
+    }
+    else  {
+        std::cout << "Try to enter a special character, you fucking idiot!!!\n"; return ;
+    }
+
     std::cout << pScore ;
 }
 
@@ -23,7 +69,7 @@ int main() {
     
     std::cout << "Enter Your Password: ";
     std::getline(std::cin, password);
-
+    
     checkPassword(password);
 
     return 0;
