7306164fa5c424552619f81c92c6fcb695942c1e
Thatipamula Sasi Kiran <sasitsk0804@gmail.com>
2026-06-27 17:42:44 +0530

    Implement password validation logic in checkPassword function

--- Program.cpp
+++ Program.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 #include <string>
 
+void checkPassword(std::string pass){
+    int pScore = 0;
+
+    bool hasUpper = false, hasLower = false, hasDigit = false, hasSpecial = false;
+
+    if (pass.length() >= 8){
+        pScore++;
+    }
+    else {
+        std::cout << "Password you entered is too short you Dumbass!!!";
+
+        return ;
+    }
+
+    std::cout << pScore ;
+}
+
 int main() {
+    std::string password;
     
+    std::cout << "Enter Your Password: ";
+    std::getline(std::cin, password);
+
+    checkPassword(password);
+
     return 0;
 }
